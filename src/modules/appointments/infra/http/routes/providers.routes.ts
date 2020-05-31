b47df30b091c46d525175c468bec8f96212e06ad
Rodrigo Grassi <rodrigo@Rodrigos-MacBook.local>
2020-05-31 20:10:19 -0300

    chore: routes from provider availability

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,12 +1,24 @@
 import { Router } from 'express';
 import ensureAuthenticaded from '@modules/users/infra/http/middlewares/ensureAuthenticaded';
 import ProvidersController from '@modules/appointments/infra/http/controllers/ProvidersController';
+import ProviderMonthAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController';
+import ProviderDayAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderDayAvailabilityController';
 
 const router = Router();
 const providersController = new ProvidersController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
 
 router.use(ensureAuthenticaded);
 
 router.get('/', providersController.index);
+router.get(
+  '/:provider_id/month-availability',
+  providerMonthAvailabilityController.index,
+);
+router.get(
+  '/:provider_id/day-availability',
+  providerDayAvailabilityController.index,
+);
 
 export default router;
