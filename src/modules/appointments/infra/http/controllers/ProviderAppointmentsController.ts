f6aeddd8c264f242baec3e1d7eea74dea4ebb8a8
Rodrigo Grassi <rodrigo@Rodrigos-MacBook.local>
2020-06-09 17:05:19 -0300

    security: limit multiple requests

--- src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -4,7 +4,7 @@ import ListProviderAppointmentsService from '@modules/appointments/services/List
 
 export default class AppointmentsController {
   public async index(request: Request, response: Response): Promise<Response> {
-    const { day, month, year } = request.body;
+    const { day, month, year } = request.query;
     const provider_id = request.user.id;
 
     const listProviderAppointments = container.resolve(
@@ -13,9 +13,9 @@ export default class AppointmentsController {
 
     const appointments = await listProviderAppointments.execute({
       provider_id,
-      day,
-      month,
-      year,
+      day: Number(day),
+      month: Number(month),
+      year: Number(year),
     });
 
     return response.json(appointments);
